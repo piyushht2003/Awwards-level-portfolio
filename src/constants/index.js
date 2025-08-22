d0cc4e4c50a849c4c04fd20f88f8d7c3c243a251
Piyush <piyushsinghthakur128@gmail.com>
2025-08-22 18:06:54 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,163 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Microservices, Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "Frontend Development",
+    description:
+      "Your product deserves an interface that feels fast, intuitive, and delightful. I craft responsive, accessible, and performance-driven UIs—turning ideas into pixel-perfect experiences users love.",
+    items: [
+      {
+        title: "Modern Frameworks",
+        description: "(React, Next.js, Vue, TypeScript)",
+      },
+      {
+        title: "UI/UX Craftsmanship",
+        description: "(Responsive Layouts, Accessibility, Micro-animations)",
+      },
+      {
+        title: "Performance & SEO",
+        description:
+          "(Core Web Vitals, Lighthouse Optimization, Best Practices)",
+      },
+    ],
+  },
+  {
+    title: "UI/UX & Design Systems",
+    description:
+      "A beautiful app isn’t just about looks—it’s about usability. I design and implement user-friendly flows, reusable components, and scalable design systems that align with your brand.",
+    items: [
+      {
+        title: "Wireframing & Prototyping",
+        description: "(Figma to Code, Rapid Iterations)",
+      },
+      {
+        title: "Design Systems",
+        description: "(Reusable Components, Consistent UI Patterns)",
+      },
+      {
+        title: "Micro-interactions",
+        description: "(GSAP, Framer Motion, Smooth Animations)",
+      },
+    ],
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    name: "Zentry MMORPG",
+    description:
+      "Welcome to Zentry — the world's largest shared adventure. This website is themed as an epic MMORPG that unites players from countless games and platforms, both digital and physical, into a unified Play Economy.An online store specializing in phone accessories including cases, chargers, cables, and power banks with MagSafe compatibility.",
+    href: "",
+    image: "/assets/projects/zentry.png",
+    bgImage: "/assets/backgrounds/zentry.webp",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "GSAP" },
+      { id: 3, name: "Node.js" },
+      // { id: 4, name: "MongoDB" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Spylt",
+    description:
+      "A stunning, award-winning website built with GSAP animations, React, and modern web technologies.",
+    href: "",
+    image: "/assets/projects/spylt (2).png",
+    bgImage: "/assets/backgrounds/spylt.png",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "GSAP" },
+      { id: 3, name: "Node.js" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 3,
+    name: "QuicStay - Hotel Booking Platform",
+    description:
+      "An e-commerce platform for Apple products and accessories with deals and category filtering.",
+    href: "",
+    image: "/assets/projects/quickstay-front.png",
+    bgImage: "/assets/backgrounds/heroImage.png",
+    frameworks: [
+      { id: 1, name: "MERN Stack" },
+      { id: 2, name: "Clerk" },
+      { id: 3, name: "Stripe" },
+      { id: 4, name: "Bootstrap" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Lazarev Agency",
+    description:
+      "This is a responsive clone of the Lazarev.Agency website built using HTML, CSS, and JavaScript. This project was created for learning purposes to explore modern UI/UX principles, animations, and smooth scrolling interactions.",
+    href: "",
+    image: "/assets/projects/laravez.png",
+    bgImage: "/assets/backgrounds/laravez-bg.png",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "GSAP" },
+      // { id: 3, name: "MySQL" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  // {
+  //   id: 5,
+  //   name: "Home Decor Marketplace",
+  //   description:
+  //     "A curated collection of designer home decor items, including furniture and artisan vases.",
+  //   href: "",
+  //   image: "/assets/projects/home-decor-store.jpg",
+  //   bgImage: "/assets/backgrounds/table.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "Angular" },
+  //     { id: 2, name: "Firebase" },
+  //     { id: 3, name: "GraphQL" },
+  //     { id: 4, name: "Material UI" },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   name: "Digital Game Store",
+  //   description:
+  //     "A gaming platform featuring discounted titles, top sellers, and genre-based browsing.",
+  //   href: "",
+  //   image: "/assets/projects/game-store.jpg",
+  //   bgImage: "/assets/backgrounds/curtains.jpg",
+  //   frameworks: [
+  //     { id: 1, name: "Svelte" },
+  //     { id: 2, name: "Node.js" },
+  //     { id: 3, name: "MongoDB" },
+  //     { id: 4, name: "Chakra UI" },
+  //   ],
+  // },
+];
+export const socials = [
+  { name: "Instagram", href: "https://www.instagram.com/piyush_singh.01/" },
+
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/piyush-singh-thakur-4ab855231/",
+  },
+  { name: "GitHub", href: "https://github.com/piyushht2003" },
+];
---
+++ src/sections/About.jsx
@@ -0,0 +1,63 @@
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `Driven by a love for building impactful digital products,
+   I create fast, user-friendly,
+    and scalable solutions tailored for growth.`;
+  const aboutText = `Obsessed with crafting sleek, responsive web experiences—from smooth GSAP animations to scalable MERN backends. Every project I build aims for one thing: impact users can feel.
+When I’m not coding:
+⚡ Experimenting with UI/UX micro-interactions & sharing them on my portfolio playground
+📚 Mentoring peers & learning new tech—because growth is a team sport
+⚽ Playing football (teamwork + strategy, just like building products)
+☕ Brewing chai while dreaming up my next side project`;
+  const imgRef = useRef(null);
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Cod with purpose, Built to scale"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/pp.jpg"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;
